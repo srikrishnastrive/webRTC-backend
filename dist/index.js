7125a3719599e00628510307990d9bd93e4a26b6
srikrishnastrive <srikrishnamuddangala@gmail.com>
2024-08-03 08:48:44 +0530

    added the basic scoket set up

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,28 @@
 "use strict";
-console.log("hello world --welcome to programming");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const serverConfig_1 = __importDefault(require("./config/serverConfig"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+const server = http_1.default.createServer(app);
+const io = new socket_io_1.Server(server, {
+    cors: {
+        methods: ["GET", "POST"],
+        origin: "*",
+    },
+});
+io.on("connection", (socket) => {
+    console.log("new user connected");
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+});
+server.listen(serverConfig_1.default.PORT, () => {
+    console.log(`server started at port ${serverConfig_1.default.PORT}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("hello world --welcome to programming");
+import cors from "cors";
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import ServerConfig from "./config/serverConfig";
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        methods: ["GET", "POST"],
+        origin: "*",
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("new user connected");
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+});
+
+server.listen(ServerConfig.PORT, () => {
+    console.log(`server started at port ${ServerConfig.PORT}`);
+});
